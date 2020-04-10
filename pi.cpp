17390ff0d201a01b4e54e9257354d97a13bba59a
PauloVLB <vitorlimaborges@gmail.com>
2020-04-09 21:12:05 -0300

    Input

--- pi.cpp
+++ pi.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -12,12 +13,15 @@ int main()
 {
     
     srand(time(NULL));
-    double nPoints;
-    cin >> nPoints;
+    double power;
+
+    cout << "Number of points = 10^";
+    cin >> power;
     
+    double nPoints = pow(10.0, power);
     double inPoints;
     
-    for(int i = 0; i < nPoints; i++){
+    for(unsigned long long int i = 0; i < nPoints; i++){
         double x = randNum(0,1);
         double y = randNum(0,1);
         
@@ -27,8 +31,7 @@ int main()
     }
         
     double pi = 4 * (inPoints/nPoints);
-    
-    cout << pi << endl;
+    cout << fixed << setprecision(power) << pi << endl;
 
     return 0;
 }
